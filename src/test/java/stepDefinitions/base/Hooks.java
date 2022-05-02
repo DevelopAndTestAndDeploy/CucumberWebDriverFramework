0c53acd23b44ba196ca22a4335a4a5b6af3f9839
chamaraf <developandtestanddeploy@gmail.com>
2022-05-02 19:28:14 +0100

    testng.xml and surefire plugin added

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -4,15 +4,14 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 
 import java.sql.Timestamp;
 
-import static driver.DriverFactory.*;
-import static java.lang.System.*;
+import static driver.DriverFactory.cleanUpDriver;
+import static driver.DriverFactory.getDriver;
+import static java.lang.System.currentTimeMillis;
 
 public class Hooks {
 
